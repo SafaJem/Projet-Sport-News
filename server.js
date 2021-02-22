0b0257897cb97c4362aeafc627e8cc5f0922ff8f
saifmogaadi12 <aboura.joujou@outlook.es>
2021-02-22 23:15:52 +0100

    dep

--- server.js
+++ server.js
@@ -15,7 +15,21 @@ app.use(express.json());
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
- 
+const whitelist = ['http://localhost:3000'​, 'http://localhost:8080'​, 'https://shrouded-journey-38552.heroku'​]
+const corsOptions = {
+  origin: function (origin, callback) {
+    console.log("** Origin of request " + origin)
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      console.log("Origin acceptable")
+      callback(null, true)
+    } else {
+      console.log("Origin rejected")
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+app.use(cors(corsOptions))
+
 // Set EJS as templating engine 
 //app.set("view engine", "ejs");
 
@@ -27,6 +41,12 @@ app.use('/api/sport', require ('./routes/user'));
 app.use('/api/article', require ('./routes/article'));
 app.use('/api/profile', require ('./routes/profile'));
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 // Create port
 const port = process.env.PORT || 5000;
 // Launch the serveer
