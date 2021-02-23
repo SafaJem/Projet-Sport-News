ca94c72638c2aba2f2d44daf8f72047958dc4e9e
saifmogaadi12 <aboura.joujou@outlook.es>
2021-02-23 14:38:42 +0100

    deploy3

--- routes/article.js
+++ routes/article.js
@@ -194,40 +194,6 @@ router.delete("/deletecomment/:_id/:index/", isAuth, async (req, res) => {
   catch (error){ 
      res.status(500).send("Server Error !"); }//{ $pull: { results: { score: 8 , item: "B" } } }
 });
-/*
-router.delete('/comment/:id/:comment_id', auth, async (req, res) => {
-  try {
-    const post = await Post.findById(req.params.id);
-
-    // Post out comment
-    const comment = post.comments.find(
-      (comment) => comment.id === req.params.comment_id
-    );
-
-    // Make sure comment exsits
-    if (!comment) {
-      return res.status(404).json({ msg: 'Comment does not exist' });
-    }
-    // Check user
-    if (comment.user.toString() !== req.user.id) {
-      return res.status(401).json({ msg: 'User not authorized' });
-    }
-    // Get remove index
-    const removeIndex = post.comments
-      .map((comment) => comment.user.toString())
-      .indexOf(req.user.id);
-
-    post.comments.splice(removeIndex, 1);
-
-    await post.save();
-
-    res.json(post.comments);
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send('Server Error');
-  }
-});
-*/
 
 //edit a comment 
 // private acces
